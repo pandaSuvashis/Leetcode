9a52043035763b55357894ecd62c08bd4229e90c
pandaSuvashis <suvashispanda18@gmail.com>
2026-07-13 12:23:42 +0530

    Time: 2 ms (98.09%), Space: 43.2 MB (96.51%) - LeetHub

---
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean uniqueOccurrences(int[] arr) {
+        HashMap<Integer , Integer> map =  new HashMap<>();
+        
+        for( int i : arr){
+            map.put(i, map.getOrDefault(i,0)+1);
+        }
+
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int freq : map.values()){
+
+            if(set.contains(freq))
+                return false;
+
+            set.add(freq);
+        }
+
+
+        return true;
+    }
+}

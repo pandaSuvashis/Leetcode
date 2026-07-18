500582308fc622502adc20c09a1d30805a285c80
pandaSuvashis <suvashispanda18@gmail.com>
2026-07-18 23:42:30 +0530

    Time: 1 ms (99.96%), Space: 77.1 MB (82.88%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int sum =0;
+        int Maxi = Integer.MIN_VALUE;
+
+        for(int i =0 ; i<nums.length; i++){
+            sum += nums[i];
+            if(sum > Maxi) Maxi =sum;
+            if(sum < 0) sum= 0;
+        }
+        return Maxi;
+    }
+}

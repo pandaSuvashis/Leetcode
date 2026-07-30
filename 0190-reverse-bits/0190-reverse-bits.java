684dca2e5c0f6af3cdff73e30b43cac279dc3e75
pandaSuvashis <suvashispanda18@gmail.com>
2026-07-30 13:00:48 +0530

    Time: 0 ms (100%), Space: 42.7 MB (9.12%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.java
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -1,14 +1,10 @@
 class Solution {
     public int reverseBits(int n) {
-        int bits[] =  new int[32];
-        for(int i=0;  i<bits.length ;i++){
-            bits[i] = n&1;
-            n >>>=1;
-        }
-
-        int ans=0;
-        for(int i=0;i<bits.length;i++){
-            ans =(ans <<1 ) | bits[i];
+        int ans =0;
+        for(int i=0; i<32 ;i++){
+            ans= ans << 1;
+            ans =ans |(n&1);
+            n>>>=1;
         }
         return ans;
     }
